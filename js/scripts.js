7eaaa0ee69f7c0bf6cf117dfcf695751b7080e0b
Bradley O'Neill <bradley.oneill@gmail.com>
2019-05-13 17:53:16 +0100

    Removed comments after creating working loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,14 @@
-//objects
+//setting array
+var repository = [];
+
+//setting objects
 var pok1 = {name:"Pikachu", height: 0.4, type: ["Eletric"]};
 var pok2 = {name:"Gengar", height: 1.5, type: ["Ghost", "Poison"]};
 var pok3 = {name:"Crawdaunt", height: 1.1, type: ["Dark", "Water"]};
 
-//array
-var repository = [pok1, pok2, pok3];
+repository = [pok1, pok2, pok3];
 
-
-
-/*
-document.write('<p>' + pok1.name + ' (height: ' + pok1.height + 'm)</p>');
-document.write('<p>' + pok2.name + ' (height: ' + pok2.height + 'm)</p>');
-document.write('<p>' + pok3.name + ' (height: ' + pok3.height + 'm)</p>');
-*/
+//loop
+for (var i = 0; i <= 3; i++) {
+document.write('<p>' + repository[i].name + ' (height: ' + repository[i].height + 'm)</p>');
+}
