876946bc752c197535af304ce069718511893697
Bradley O'Neill <bradley.oneill@gmail.com>
2019-05-13 18:25:52 +0100

    Formatted for readability and added normalized.css file

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,25 @@
 var repository = [];
 
 //setting objects
-var pok1 = {name:"Pikachu", height: 0.4, type: ["Eletric"]};
-var pok2 = {name:"Gengar", height: 1.5, type: ["Ghost", "Poison"]};
-var pok3 = {name:"Crawdaunt", height: 1.1, type: ["Dark", "Water"]};
+var pok1 = {
+	name: "Pikachu",
+	height: 0.4,
+	type: ["Eletric"]
+};
+var pok2 = {
+	name: "Gengar",
+	height: 1.5,
+	type: ["Ghost", "Poison"]
+};
+var pok3 = {
+	name: "Crawdaunt",
+	height: 1.1,
+	type: ["Dark", "Water"]
+};
 
 repository = [pok1, pok2, pok3];
 
 //loop
-for (var i = 0; i <= 3; i++) {
-document.write('<p>' + repository[i].name + ' (height: ' + repository[i].height + 'm)</p>');
+for (var i = 0; i <= 2; i++) {
+	document.write('<p>' + repository[i].name + ' (height: ' + repository[i].height + 'm)</p>');
 }
