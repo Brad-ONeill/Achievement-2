3b602631740c51e3fb942fe717af255cbd136803
Bradley O'Neill <bradley.oneill@gmail.com>
2019-05-13 17:22:54 +0100

    Managed to get static results to show on page.
    Commented out static document.write code (that actually works) for reference when writing for loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,15 @@
-//array
-var repository = ["pok1", "pok2", "pok3"];
-
 //objects
 var pok1 = {name:"Pikachu", height: 0.4, type: ["Eletric"]};
 var pok2 = {name:"Gengar", height: 1.5, type: ["Ghost", "Poison"]};
 var pok3 = {name:"Crawdaunt", height: 1.1, type: ["Dark", "Water"]};
 
-for ();
+//array
+var repository = [pok1, pok2, pok3];
+
+
+
+/*
+document.write('<p>' + pok1.name + ' (height: ' + pok1.height + 'm)</p>');
+document.write('<p>' + pok2.name + ' (height: ' + pok2.height + 'm)</p>');
+document.write('<p>' + pok3.name + ' (height: ' + pok3.height + 'm)</p>');
+*/
