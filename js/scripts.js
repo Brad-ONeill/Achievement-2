e1e00fc700019a051b76eba80ec64450b5f436f2
Bradley O'Neill <bradley.oneill@gmail.com>
2019-05-13 17:05:16 +0100

    Created repository of arrays and object

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
-var randomNumber = (Math.random() * 6) + 1;
-var diceRoll = Math.floor(randomNumber);
-document.write(diceRoll);
+//array
+var repository = ["pok1", "pok2", "pok3"];
+
+//objects
+var pok1 = {name:"Pikachu", height: 0.4, type: ["Eletric"]};
+var pok2 = {name:"Gengar", height: 1.5, type: ["Ghost", "Poison"]};
+var pok3 = {name:"Crawdaunt", height: 1.1, type: ["Dark", "Water"]};
+
+for ();
