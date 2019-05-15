93ba058142503c3162fbce79431afce99d1b2b61
Bradley O'Neill <bradley.oneill@gmail.com>
2019-05-15 13:01:16 +0100

    Changed code based on tutor's feedback
    Tutor based feedback to future proof code and apply more direct stylings

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,25 @@
 //setting objects
-var pok1 = {
-	name: "<h2>Pikachu",
-	height: 0.4,
-	type: ["Eletric"]
-};
-var pok2 = {
-	name: "<h2>Gengar",
-	height: 1.5,
-	type: ["Ghost", "Poison"]
-};
-var pok3 = {
-	name: "<h2>Crawdaunt",
-	height: 1.1,
-	type: ["Dark", "Water"]
-};
-
-repo = [pok1, pok2, pok3];
+var repo = [
+	{
+		name: "Pikachu",
+		height: 0.4,
+		type: ["Eletric"]
+},
+	{
+		name: "Gengar",
+		height: 1.5,
+		type: ["Ghost", "Poison"]
+},
+	{
+		name: "Crawdaunt",
+		height: 1.1,
+		type: ["Dark", "Water"]
+}
+];
 
 //loop
-for (var i = 0; i <= 2; i++) {
-	document.write(repo[i].name + ' (height: ' + repo[i].height + 'm)</h2>');
+for (var i = 0; i < repo.length; i++) {
+	document.write('<h2>' + repo[i].name + ' (height: ' + repo[i].height + 'm)</h2>');
 
 	if (repo[i].height > 1.4) {
 		document.write('<b>&nbsp;-&nbsp;Woah! Look at this chonky boi.</b>')
