6776db9156ff726d6887642a5fa6068c5cb2992d
Bradley O'Neill <bradley.oneill@gmail.com>
2019-05-22 10:42:37 +0100

    Added IF statement to single out the chungus

--- js/scripts.js
+++ js/scripts.js
@@ -21,21 +21,8 @@ repo.forEach(function (entry) {
 	document.write('<h2>' + entry.name + '</h2><br>Height: ' + entry.height + 'm<br><br>');
 
 	console.log(entry);
-
-});
-
-
-
-
-
-
-
-/*loop
-for (var i = 0; i < repo.length; i++) {
-	document.write('<h2>' + repo[i].name + ' (height: ' + repo[i].height + 'm)</h2>');
-
-	if (repo[i].height > 1.4) {
+	if (entry.height > 1.4) {
 		document.write('<b>&nbsp;-&nbsp;Woah! Look at this chonky boi.</b>')
 	}
-}
-*/
+});
+
