b5935d2525e77b21b4fb847df058f12aef6c3bcb
Bradley O'Neill <bradley.oneill@gmail.com>
2019-05-22 22:20:55 +0100

    Created IIFE for repository and made a comment copy of forEach loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,40 @@
-//setting objects
-var repo = [
-	{
-		name: "Pikachu",
-		height: 0.4,
-		type: ["Eletric"]
-	},
-	{
-		name: "Gengar",
-		height: 1.5,
-		type: ["Ghost", "Poison"]
-	},
-	{
-		name: "Crawdaunt",
-		height: 1.1,
-		type: ["Dark", "Water"]
-	}
-];
+//New global variable
+var pokemonRepository = (function () {
+	//this is now a local variable
+	var repo = [
+		{
+		  name: "Pikachu",
+		  height: 0.4,
+		  type: ["Eletric"]
+		},
+		{
+		  name: "Gengar",
+		  height: 1.5,
+		  type: ["Ghost", "Poison"]
+		},
+		{
+		  name: "Crawdaunt",
+		  height: 1.1,
+		  type: ["Dark", "Water"]
+		}
 
-repo.forEach(function(entry) {
+	];
+
+})(); //end of IIFE
+
+repo.forEach(function (entry) {
+	document.write('<h2>' + entry.name + '</h2><br>Height: ' + entry.height + 'm<br><br>');
+
+
+	if (entry.height > 1.4) {
+		document.write('<b>&nbsp;-&nbsp;Woah! Look at this chonky boi.</b>')
+	}
+});
+
+/*
+--- previous loop for reference ---
+
+repo.forEach(function (entry) {
 	document.write('<h2>' + entry.name + '</h2><br>Height: ' + entry.height + 'm<br><br>');
 
 	console.log(entry);
@@ -25,15 +42,4 @@ repo.forEach(function(entry) {
 		document.write('<b>&nbsp;-&nbsp;Woah! Look at this chonky boi.</b>')
 	}
 });
-
-/*
-function printArrayDetails(pokemonList) {
-	for (var i = 0; i < pokemonList.length; i++) {
-		document.write('<h2>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + 'm)</h2>');
-
-		if (pokemonList[i].height > 1.4) {
-			document.write('<b>&nbsp;-&nbsp;Woah! Look at this chonky boi.</b>');
-		}
-	}
-}
 */
