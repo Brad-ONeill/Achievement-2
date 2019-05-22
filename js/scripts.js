204d24cb56733cb0cc9c6c959b4e027756a5e78d
Bradley O'Neill <bradley.oneill@gmail.com>
2019-05-22 10:41:56 +0100

    Minor code changes to get functionality

--- js/scripts.js
+++ js/scripts.js
@@ -4,20 +4,33 @@ var repo = [
 		name: "Pikachu",
 		height: 0.4,
 		type: ["Eletric"]
-},
+	},
 	{
 		name: "Gengar",
 		height: 1.5,
 		type: ["Ghost", "Poison"]
-},
+	},
 	{
 		name: "Crawdaunt",
 		height: 1.1,
 		type: ["Dark", "Water"]
-}
+	}
 ];
 
-//loop
+repo.forEach(function (entry) {
+	document.write('<h2>' + entry.name + '</h2><br>Height: ' + entry.height + 'm<br><br>');
+
+	console.log(entry);
+
+});
+
+
+
+
+
+
+
+/*loop
 for (var i = 0; i < repo.length; i++) {
 	document.write('<h2>' + repo[i].name + ' (height: ' + repo[i].height + 'm)</h2>');
 
@@ -25,3 +38,4 @@ for (var i = 0; i < repo.length; i++) {
 		document.write('<b>&nbsp;-&nbsp;Woah! Look at this chonky boi.</b>')
 	}
 }
+*/
