e7f79965bae39d3abcd22ad453ea255c761303bb
Bradley O'Neill <bradley.oneill@gmail.com>
2019-06-03 22:42:18 +0100

    Finalised code for 1.6
    removed the <li> elements from index.html
    
    Added:
    'button' along with event listener

--- js/scripts.js
+++ js/scripts.js
@@ -30,25 +30,38 @@ var pokemonRepository = (function () {
 	}
 
 	function addListItem(entry) {
+
 		//defining li and class
 		var $li = document.createElement('li');
 		$li.classList.add('pokedex_item');
+
 		//assigning li to ul parent
 		var $ul = document.querySelector('ul');
 		$ul.appendChild($li);
 
+		//button creation
 		var $info_button = document.createElement('button');
-		$info_button.classList.add('p-button');	
+		$info_button.classList.add('p-button');
 		$info_button.innerHTML = entry.name;
 		$li.appendChild($info_button);
-		
+
+		//event listener
+		$info_button.addEventListener('click', function (event) {
+		console.log(entry.name)				
+		});
+
+	}
+	
+	function showDetails(pokemon) {
+		console.log(pokemon);
 	}
 
 	//function returns
 	return {
 		add: add,
 		getALL: getALL,
-		addListItem: addListItem
+		addListItem: addListItem,
+		showDetails: showDetails
 
 	};
 
@@ -60,6 +73,6 @@ var pmon = pokemonRepository.getALL();
 
 console.log(pmon);
 
-pmon.forEach(function (entry, index) {
+pmon.forEach(function (entry) {
 	pokemonRepository.addListItem(entry);
 });
