7f5815057c7b8e5271d9ea416fac2feaac23bcf4
Bradley O'Neill <bradley.oneill@gmail.com>
2019-07-06 02:43:44 +0100

    Added code from scratch and commented out to understand better

--- js/scripts.js
+++ js/scripts.js
@@ -11,7 +11,7 @@ var pokemonRepository = (function () {
 	}
 
 	function loadList() {
-		return fetch(apiUrl).then(function (response) {
+		return fetch(apiUrl).then(function (response) {					
 			return response.json();
 		}).then(function (json) {
 			json.results.forEach(function (item) {
@@ -28,6 +28,7 @@ var pokemonRepository = (function () {
 
 	function loadDetails(item) {
 		var url = entry.detailsUrl;
+
 		return fetch(url).then(function (response) {
 			return response.json();
 		}).then(function (details) {
@@ -41,19 +42,23 @@ var pokemonRepository = (function () {
 
 	function addListItem(entry) {
 
-		//defining li and class
-		var $li = document.createElement('li');
-		$li.classList.add('pokedex_item');
-		//assigning li to ul parent
-		var $ul = document.querySelector('ul');
-		$ul.appendChild($li);
-		//button creation
-		var $info_button = document.createElement('button');
-		$info_button.classList.add('info-button');
-		$info_button.innerHTML = entry.name;
-		$li.appendChild($info_button);
+		//step 1
+		var $li = document.createElement('li'); 							// creates li element
+		$li.classList.add('pokedex_item'); 									// add the class 'pokedex_item' to the li element
+
+		//step 2
+		var $ul = document.querySelector('ul'); 							//select ul from the document
+		$ul.appendChild($li); 												// add the li element from step 1 to the ul element
+
+		//step 3
+		var $info_button = document.createElement('button'); 				// create the 'button' element
+		$info_button.classList.add('info-button'); 							// add 'info-button' as a class to the button element
+		$info_button.setAttribute('id', "modal-button");					// add 'modal-button' as an id to the button lement
+		$info_button.innerHTML = entry.name; 								// add the entry name of the PokéMon to the button element
+		$li.appendChild($info_button);										// add the button to the li element
+
 		//event listener
-		$info_button.addEventListener('click', function (event) {
+		$info_button.addEventListener('click', function (event) { 			// listening for a click action on the button to add name and details URL to the console
 			console.log(entry.name, entry.detailsUrl);
 		});
 
@@ -70,7 +75,9 @@ var pokemonRepository = (function () {
 	};
 
 })();
-//---
+
+
+
 pokemonRepository.loadList().then(function () {
 	// Now the data is loaded!
 	pmon.forEach(function (entry) {
